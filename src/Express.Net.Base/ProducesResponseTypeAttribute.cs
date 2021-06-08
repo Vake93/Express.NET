248863e8bc76caf2041938b394e9337c2505ea53
Vishvaka Ranasinghe <vishvaka@gmail.com>
2021-06-08 13:28:48 +0530

    Create ProducesResponseTypeAttribute.cs

---
+++ src/Express.Net.Base/ProducesResponseTypeAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Express.Net
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class ProducesResponseTypeAttribute : Attribute
+    {
+        public ProducesResponseTypeAttribute(int statusCode)
+        {
+            StatusCode = statusCode;
+        }
+
+        public ProducesResponseTypeAttribute(Type type, int statusCode)
+        {
+            Type = type;
+            StatusCode = statusCode;
+        }
+
+        public int StatusCode { get; set; }
+
+        public Type? Type { get; set; }
+    }
+}
