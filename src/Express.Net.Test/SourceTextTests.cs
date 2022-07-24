fd847248bca332fc0efc3bd712e3a98229f00227
Vishvaka Ranasinghe <vishvaka@gmail.com>
2022-07-24 10:20:29 +0530

    Create SourceTextTests.cs

---
+++ src/Express.Net.Test/SourceTextTests.cs
@@ -0,0 +1,17 @@
+﻿using Express.Net.CodeAnalysis.Text;
+using Xunit;
+
+namespace Express.Net.Test;
+
+public class SourceTextTests
+{
+    [Theory]
+    [InlineData(".", 1)]
+    [InlineData(".\r\n", 2)]
+    [InlineData(".\r\n\r\n", 3)]
+    public void IncludesLastLine(string text, int expectedLineCount)
+    {
+        var sourceText = SourceText.From(text);
+        Assert.Equal(expectedLineCount, sourceText.Lines.Length);
+    }
+}
